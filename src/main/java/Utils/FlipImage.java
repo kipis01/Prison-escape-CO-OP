3f1ccb6187185715bdcfb1e8cbbda19a384d29d5
sandis <sandis@xubuntu.myguest.virtualbox.org>
2023-05-26 18:59:13 +0300

    Cleanup

--- src/main/java/Utils/FlipImage.java
+++ src/main/java/Utils/FlipImage.java
@@ -12,16 +12,14 @@ public class FlipImage {
 		BufferedImage flippedImage = new BufferedImage(width, height, image.getType());
 		Graphics2D g2d = flippedImage.createGraphics();
 
-		// Create an AffineTransform for flipping
 		AffineTransform transform = new AffineTransform();
 		transform.scale(-1, 1);
 		transform.translate(-width, 0);
 
-		// Apply the transform and draw the original image onto the new image
 		g2d.setTransform(transform);
 		g2d.drawImage(image, 0, 0, null);
 
-		g2d.dispose(); // Dispose of the graphics object
+		g2d.dispose();
 
 		return flippedImage;
 	}
